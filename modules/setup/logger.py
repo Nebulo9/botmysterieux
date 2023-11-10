edb98a724c9554e98a73a6b4b6961e441930fe69
Nebulo9 <matdessauv@gmail.com>
2023-11-10 21:20:31 +0100

    Changed logging file handler opening mode

--- modules/setup/logger.py
+++ modules/setup/logger.py
@@ -3,6 +3,6 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(asctime)s: %(name)s: %(message)s')
 LOGGER = logging.getLogger('botmysterieux')
 LOGGER.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='botmafieux.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='botmafieux.log', encoding='utf-8')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 LOGGER.addHandler(handler)
